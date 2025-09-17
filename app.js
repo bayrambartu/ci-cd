68198c3f2eeeeee1d798abe51bc98f9d892b76d7
bayrambartu <kurnazb32@gmail.com>
2025-09-17 10:14:28 +0300

    Mesajı güncelledim, deploy test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD! This is my first automated deployment!');
+  res.send('hii CI/CD! This is my first automated deployment!');
 });
 
 app.listen(port, () => {
